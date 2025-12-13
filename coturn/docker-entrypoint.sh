1d42285ca9abc5c6e83c7a5b46419924a6ba5cf4
Denis Moiseev <lobziiko.o@gmail.com>
2025-12-13 01:07:27 +0100

    Update Coturn entrypoint script to use temporary config file
    - Redirect generated configuration to `/tmp/turnserver.conf` for better file management.
    - Adjust turnserver execution to reference the temporary config file.

--- coturn/docker-entrypoint.sh
+++ coturn/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Substitute environment variables in the template config
-envsubst < /etc/coturn/turnserver.conf.template > /etc/coturn/turnserver.conf
+envsubst < /etc/coturn/turnserver.conf.template > /tmp/turnserver.conf
 
 # Execute coturn with passed arguments
-exec turnserver "$@"
+exec turnserver -c /tmp/turnserver.conf "$@"
